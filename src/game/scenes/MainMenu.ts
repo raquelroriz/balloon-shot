54c95f15d48a3dfdb7e6cc18aa72204f6e701e4c
Raquel Roriz <maria.raquel.roriz@gmail.com>
2025-04-15 21:14:01 +0200

    partial: The Platforms
    https://docs.phaser.io/phaser/getting-started/making-your-first-phaser-game

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -1,10 +1,13 @@
 import { GameObjects, Scene } from 'phaser';
 
 import { EventBus } from '../EventBus';
+import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
 
 export class MainMenu extends Scene
 {
     background: GameObjects.Image;
+    star: GameObjects.Image;
+    ground: GameObjects.Image;
     logo: GameObjects.Image;
     title: GameObjects.Text;
     logoTween: Phaser.Tweens.Tween | null;
@@ -16,16 +19,16 @@ export class MainMenu extends Scene
 
     create ()
     {
+
         this.background = this.add.image(512, 384, 'background');
-
-        this.logo = this.add.image(512, 300, 'logo').setDepth(100);
-
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-
+        this.star = this.add.image(400, 300, 'star');
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+        
+        
         EventBus.emit('current-scene-ready', this);
     }
     
